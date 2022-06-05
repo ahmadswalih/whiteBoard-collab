121601c8a2e09508c7b1f5451033787266427b45
ahmad swalih <ahmadswalih777@gmail.com>
2022-06-05 09:48:35 +0530

    Cors problem solved with adding origin in the server folder

--- Server/server.js
+++ Server/server.js
@@ -1,16 +1,17 @@
-const app = require("express")();
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http,  { cors: { origin: '*' } });
 
-io.on("connection",(socket)=>{
-    console.log("User is online");
 
-    socket.on('canvas-data',(data)=>{
-        socket.broadcast.emit("canvas-data",data)
-    })
+io.on('connection', (socket)=> {
+
+      socket.on('canvas-data', (data)=> {
+            socket.broadcast.emit('canvas-data', data);
+            
+      })
 })
 
-const Port = 5000
-http.listen(Port,()=>{
-    console.log(`Server is up and Running ${Port}`);
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
 })
